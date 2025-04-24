81cfc127da0ecc8ef708ffc001e646e3b7e4de07
GettingFunky <sagemmyV75@yahoo.gr>
2025-04-24 18:22:13 +0300

    Ïcompleted all student controllers. Student Delete, Insert, Inserted, View, Update, Updated, ViewAll

--- src/main/java/gr/aueb/cf/schoolapp/controller/StudentViewController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/StudentViewController.java
@@ -1,11 +1,11 @@
 package gr.aueb.cf.schoolapp.controller;
 
-import gr.aueb.cf.schoolapp.dao.IStudentDAO;
-import gr.aueb.cf.schoolapp.dao.StudentDAOImpl;
+import gr.aueb.cf.schoolapp.dao.*;
+import gr.aueb.cf.schoolapp.dto.StudentReadOnlyDTO;
 import gr.aueb.cf.schoolapp.exceptions.StudentDAOException;
 import gr.aueb.cf.schoolapp.exceptions.StudentNotFoundException;
-import gr.aueb.cf.schoolapp.service.IStudentService;
-import gr.aueb.cf.schoolapp.service.StudentServiceImpl;
+import gr.aueb.cf.schoolapp.model.City;
+import gr.aueb.cf.schoolapp.service.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,32 +13,37 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
-@WebServlet("/school-app/students/delete")
-public class StudentDeleteController extends HttpServlet {
+@WebServlet("/school-app/students/show")
+public class StudentViewController extends HttpServlet {
 
     IStudentDAO studentDAO = new StudentDAOImpl();
     IStudentService studentService = new StudentServiceImpl(studentDAO);
+    ICityDAO cityDAO = new CityDAOImpl();
+    ICityService cityService = new CityServiceImpl(cityDAO);
     String message = "";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
         int id = Integer.parseInt(req.getParameter("id"));
-        String role = (String) req.getSession().getAttribute("role");
-        if (!"ADMIN".equals(role)) {
-            resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Απαγορεύεται η πρόσβαση.");
-            return;
-        }
 
         try {
-            studentService.deleteStudent(id);
-            req.setAttribute("id", id);
-            req.getRequestDispatcher("/WEB-INF/jsp/student-deleted.jsp").forward(req, resp);
-        } catch (StudentDAOException | StudentNotFoundException e) {
+            StudentReadOnlyDTO student = studentService.getStudentById(id);
+            List<City> cities = cityService.getAllCities();
+
+            req.setAttribute("student", student);
+            req.setAttribute("cities", cities);
+
+            req.getRequestDispatcher("/WEB-INF/jsp/student-view.jsp").forward(req, resp);
+        } catch (StudentDAOException | StudentNotFoundException | SQLException e) {
             message = e.getMessage();
             req.setAttribute("message", message);
             req.getRequestDispatcher("/WEB-INF/jsp/students.jsp").forward(req, resp);
         }
     }
 }
+
